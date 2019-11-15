480b604eb22f5a4b9a81febd81c84ad6519f2e52
CanBul <canbulguoglu@gmail.com>
2019-11-15 16:08:39 +0300

    commit

---
+++ BinaryRankSearch.py
@@ -0,0 +1,20 @@
+# Finding the rank of the number in the given reversed sorted list
+
+def binary_rank_search(mylist, number):
+    length = len(mylist)
+    middle = length//2
+    left = mylist[0:middle]
+    right = mylist[middle:]
+
+    if mylist[middle] == number:
+        return middle+1
+    elif uzunluk==1 and number>mylist[middle]:
+        return middle+1
+    elif uzunluk==1 and number<mylist[middle]:
+        return middle+2
+
+    elif mylist[middle]> number:
+        return middle+binary_rank_search(right,number)
+
+    else:
+        return binary_rank_search(left,number)
